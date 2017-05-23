590cee028aa400d325c4921cfb7e95ad442f4df0
Sallé <joaosalle@id.uff.br>
2017-05-23 20:03:48 -0300

    teste

---
+++ src/main/java/com/trabalhoDW/trabalhoDW/TrabalhoDwApplication.java
@@ -0,0 +1,12 @@
+package com.trabalhoDW.trabalhoDW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoDwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoDwApplication.class, args);
+	}
+}
