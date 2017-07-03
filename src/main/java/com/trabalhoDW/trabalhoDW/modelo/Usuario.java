63be2664a6c6225ca220ad02e2e7edbf7e76f6f9
Sallé <jhonnyot@gmail.com>
2017-07-03 20:51:43 -0300

    2

--- src/main/java/com/trabalhoDW/trabalhoDW/modelo/Usuario.java
+++ src/main/java/com/trabalhoDW/trabalhoDW/modelo/Usuario.java
@@ -7,7 +7,6 @@ package com.trabalhoDW.trabalhoDW.modelo;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
 import java.util.Set;
@@ -19,8 +18,6 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 
 /**
  *
